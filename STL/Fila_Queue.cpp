d0460fcf99f6fddc413d4d8247c9b78a5db1087d
Vini Cassol <vinicassol@gmail.com>
2021-09-10 20:01:32 -0300

    Update Fila_Queue.cpp

--- STL/Fila_Queue.cpp
+++ STL/Fila_Queue.cpp
@@ -1,36 +1,63 @@
 #include <iostream>
-#include <queue> // Biblioteca para Filas
+#include <stack> //Empilhar Dados (operações no topo)
+#include <queue> //Enfileirar Dados (insere no final da fila e remove no inicio da fila)
 using namespace std;
 
-//Nas filas, os dados são inseridos no final e removidos a partir do inicio (dados enfileirados)
 int main()
 {
-    //Fila de string chamada vacina. Vai organizar a fila de pessoas aguardando a vacina
-    queue<string> vacina;
+  /*  stack<string> minhaPilha;
+    queue<string> minhaFila;
     
-    vacina.push("Johann");
-    vacina.push("Eduarda");
+    // Inserção
+    minhaPilha.push("Dados"); // insere no topo
+    minhaFila.push("Dados"); // insere no fim da fila
+    
+    //Remoção
+    minhaPilha.pop(); //Remove o topo
+    minhaFila.pop(); // Remove do inicio
+    
+    //Consulta
+    minhaPilha.top(); // Consulta a partir do topo
+    minhaFila.front(); // Consulta o primeiro da fila
+    
+    //Tamanho
+    minhaPilha.size();
+    minhaFila.size();
+    
+    // Verificar se a estrutura esta vazia
+    minhaPilha.empty();
+    minhaFila.empty();
+    */
+    
+    // ###########################
+    
+    queue <string> vacina;
+    vacina.push("Bruno");
+    vacina.push("Miguel");
+    vacina.push("Nicollas");
+    vacina.push("Rafael");
     vacina.push("Victor");
-    vacina.push("Bruna");
     
-    if(vacina.empty()) // Verifica se estrutura estah vazia
-        cout << "Nao ha ninguem aguardando\n";
+    if(vacina.empty())
+        cout << "Fila vazia\n";
     else
     {
-        cout << "Pessoas na fila: " << vacina.size() << endl;
-        cout << "Primeiro da fila: " << vacina.front() << endl;
-        while(!vacina.empty()) //Enquanto a fila não estiver vazia
+        while(!vacina.empty())
         {
-            cout << "Vacina aplicada em " << vacina.front() << ". ";
-            vacina.pop(); // remove da fila
-            cout << "Proximo a ser vacinado: " << vacina.front() << endl;
+            cout << "Pessoas na fila: " << vacina.size() << endl;
+            cout << "Vacinado: " << vacina.front() << ". ";
+            vacina.pop();
+            cout << "Proximo a vacinar: " << vacina.front() << endl;
         }
     }
+    
     /* EXERCICIO:
-     -> Alterar o programa da vacina para incluir um menu onde o usuario possa:
-     1. Adicionar pessoa na fila, informando o nome, o cpf e qual vacina esta disponivel pra ela (neste momento o sistema sorteia qual vacina aplicada: Coronavac, Astrazeneca, Pfizer, Janssen);
-     2. Aplicar vacina.
-     3. Status: Informar quantas pessoas estão aguardando e quantas já foram vacinadas;
-     4. Sair: Informar o total de vacinados. Porém o programa não pode finalizar se houverem pessoas na fila aguardando.
-     */
+         -> Alterar o programa da vacina para incluir um menu onde o usuario possa:
+         1. Adicionar pessoa na fila, informando o nome, o cpf e qual vacina esta disponivel pra ela (neste momento o sistema sorteia qual vacina aplicada: Coronavac, Astrazeneca, Pfizer, Janssen);
+         2. Aplicar vacina.
+         3. Status: Informar quantas pessoas estão aguardando e quantas já foram vacinadas;
+         4. Sair: Informar o total de vacinados. Porém o programa não pode finalizar se houverem pessoas na fila aguardando.
+         */
+    
+    return 0;
 }
